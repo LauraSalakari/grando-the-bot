da8fd3972679ed688fedbb3876f7f275c0f810d4
Laura Salakari <salakari.laura@gmail.com>
2020-11-19 21:29:54 +0100

    deploy build

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+require('dotenv').config(); 
 const { default: Axios } = require('axios');
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const {prefix, token, tenor} = require("./config.json");
+// const {prefix, token, tenor} = require("./config.json");
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.login(token);
+client.login(process.env.TOKEN);
 
 client.on("message", message => {
-    if(!message.content.startsWith(prefix) || message.author.bot || message.length < 2) return;
+    if(!message.content.startsWith(process.env.PREFIX) || message.author.bot || message.length < 2) return;
     
-    const args = message.content.slice(prefix.length).trim();
+    const args = message.content.slice(process.env.PREFIX.length).trim();
     const chan = message.channel;
     getGifs(args, chan);
 })
 
 const getGifs = (arg, chan) => {
-    Axios.get(`https://api.tenor.com/v1/search?q=${arg}&key=${tenor}&limit=8`)
+    Axios.get(`https://api.tenor.com/v1/search?q=${arg}&key=${process.env.TENOR}&limit=8`)
     .then((response) => {
         if(!response.data.results.length){
             chan.send("Too random even for Grando! No gifs found.");
